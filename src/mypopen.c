5f841bf42b8b04a0dd97e7190549157ed1fda775
amq <amq@users.noreply.github.com>
2016-04-21 12:30:37 +0200

    Feature complete

--- src/mypopen.c
+++ src/mypopen.c
@@ -1,8 +1,23 @@
 #include "mypopen.h"
 
-static FILE *global_stream = NULL;
+/**
+ * a global variable containing process id returned by fork
+ */
 static pid_t pid = -1;
 
+/**
+ * a global variable containing a pointer of a pipe stream initiated by mypopen
+ */
+static FILE *global_stream = NULL;
+
+/**
+ * @brief initiate a pipe stream to or from a process
+ *
+ * @param command the command to be executed
+ * @param type the I/O mode (r/w)
+ *
+ * @returns a file pointer or NULL in case of error
+ */
 FILE *mypopen(const char *command, const char *type) {
   int pipe_ends[2];
   int parent, child;
@@ -13,13 +28,13 @@ FILE *mypopen(const char *command, const char *type) {
     return NULL;
   }
 
-  /* the command must not be empty */
+  /* check the command input */
   if (command == NULL) {
     errno = EINVAL;
     return NULL;
   }
 
-  /* only two types are allowed: r and w */
+  /* check the type input */
   if (strcmp(type, "r") == 0) {
     parent = READ;
     child = WRITE;
@@ -31,10 +46,12 @@ FILE *mypopen(const char *command, const char *type) {
     return NULL;
   }
 
+  /* create a pipe */
   if (pipe(pipe_ends) == -1) {
     return NULL;
   }
 
+  /* create a child process */
   switch (pid = fork()) {
   /* error */
   case -1:
@@ -66,10 +83,18 @@ FILE *mypopen(const char *command, const char *type) {
   return global_stream;
 }
 
+/**
+ * @brief close a pipe stream to or from a process
+ *
+ * @param stream the stream to be closed
+ *
+ * @returns the exit status of the process or -1 in case of error
+ */
 int mypclose(FILE *stream) {
+  pid_t wait_pid;
   int status;
 
-  /* check if mypopen was run */
+  /* check if mypopen was previously run */
   if (global_stream == NULL) {
     errno = ECHILD;
     return -1;
@@ -81,6 +106,7 @@ int mypclose(FILE *stream) {
     return -1;
   }
 
+  /* close the stream */
   if (fclose(stream) == EOF) {
     pid = -1;
     global_stream = NULL;
@@ -88,13 +114,25 @@ int mypclose(FILE *stream) {
     return -1;
   }
 
-  do {
-    wait(&status);
-  } while (errno == EINTR);
+  /* wait for a child process to terminate */
+  while ((wait_pid = waitpid(pid, &status, 0)) != pid) {
+    if (wait_pid == -1) {
+      if (errno == EINTR) {
+        continue;
+      }
+    }
+
+    /* reached only if waitpid returned an unexpected value or errno */
+    pid = -1;
+    global_stream = NULL;
+    errno = ECHILD;
+    return -1;
+  }
 
   pid = -1;
   global_stream = NULL;
 
+  /* check if the child process terminated normally */
   if (WIFEXITED(status) != 0) {
     return WEXITSTATUS(status);
   }
